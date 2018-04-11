c0dd9a16d74410c267e8fefa012cef2b41c86628
guowenfh <guowen921@live.com>
2018-04-11 23:41:19 +0800

    add local css module , exclude nodeModules .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import logo from './logo.png'
-import './App.css'
+import styles from './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.App}>
+        <header className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
+          <h1 className={styles['App-title']}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
