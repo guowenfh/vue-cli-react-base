70bb60ef2eeb5a78b344d34c9e95dce574cb8b8f
guowenfh <guowen921@live.com>
2018-07-11 00:50:13 +0800

    change main.js to index.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,7 +21,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './src/index.js'
   },
   output: {
     path: config.build.assetsRoot,
