6f8cc43290856fb57c3568a48e44c0aaa7cae680
guowenfh <guowen921@live.com>
2018-10-13 16:24:19 +0800

    add rematch husky

---
+++ src/models/global.js
@@ -0,0 +1,40 @@
+export default {
+  state: {
+    query: {} // 全局一些属性，比如 location.search 拿到的值
+  },
+  reducers: {
+    setQuery(state, payload) {
+      return {
+        ...state,
+        query: {
+          ...state.query,
+          ...payload
+        }
+      }
+    }
+  },
+  effects: {
+    // handle state changes with impure functions.
+    // use async/await for async actions
+    async incrementAsync(payload = 1, rootState = {}) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      this.incrementcount(payload)
+    }
+  }
+}
+
+// import { connect } from 'react-redux'
+// const mapStateToProps = state => {
+//     console.error(state)
+//     return {
+//         count: state.global.count
+//     }
+// }
+// const mapDispatchToProps = dispatch => ({
+//     incrementAsync: dispatch.global.incrementAsync
+// })
+
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(Component)
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import * as models from './models'
+import { init } from '@rematch/core'
+const store = init({ models })
+
+export default store
